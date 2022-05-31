b7b5d229577adc60c4b34f098647c9a4ad50a1a9
LAPTOP-GG32HP4K\Rustam <rustamw888@gmail.com>
2022-06-01 02:13:07 +0300

    промежуточный коммит с дефолтными значениями

--- src/test/java/io/qaguru/owner/config/WebDriverConfig.java
+++ src/test/java/io/qaguru/owner/config/WebDriverConfig.java
@@ -1,12 +1,28 @@
 package io.qaguru.owner.config;
 
+import java.util.Objects;
+
 public class WebDriverConfig {
 
-    public Browser getBrowser() {
-        return Browser.FIREFOX;
+    public String getBaseUrl() {
+        // зачитываем данные из командной строки
+        String baseUrl = System.getProperty("baseUrl");
+        // обрабатываем дефолтное значение
+        if (Objects.isNull(baseUrl)) {
+            baseUrl = "https://github.com";
+        }
+        // конвертируем в возвращаемый тип
+        return baseUrl;
     }
 
-    public String getBaseUrl() {
-        return "https://github.com";
+    public Browser getBrowser() {
+        // зачитываем данные из командной строки
+        String browser = System.getProperty("browser");
+        // обрабатываем дефолтное значение
+        if (Objects.isNull(browser)) {
+            browser = "CHROME";
+        }
+        // конвертируем в возвращаемый тип
+        return Browser.valueOf(browser);
     }
 }
