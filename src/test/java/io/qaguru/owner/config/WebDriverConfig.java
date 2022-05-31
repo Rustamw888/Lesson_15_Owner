52319a9bf3303e4b7b4eec3e3c9b072bcd024cea
LAPTOP-GG32HP4K\Rustam <rustamw888@gmail.com>
2022-06-01 02:18:45 +0300

    добавление remoteUrl

--- src/test/java/io/qaguru/owner/config/WebDriverConfig.java
+++ src/test/java/io/qaguru/owner/config/WebDriverConfig.java
@@ -1,8 +1,25 @@
 package io.qaguru.owner.config;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Objects;
 
 public class WebDriverConfig {
+
+    public URL getRemoteUrl() {
+        // зачитываем данные из командной строки
+        String remoteUrl = System.getProperty("remoteUrl");
+        // обрабатываем дефолтное значение
+        if (Objects.isNull(remoteUrl)) {
+            remoteUrl = "https://selenium:4444/wd/hub";
+        }
+        // конвертируем в возвращаемый тип
+        try {
+            return new URL(remoteUrl);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public String getBaseUrl() {
         // зачитываем данные из командной строки
