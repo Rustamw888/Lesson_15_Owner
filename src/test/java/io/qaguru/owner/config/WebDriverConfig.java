93ef6ce798297bd0a8b3c237752b686c2653f86b
LAPTOP-GG32HP4K\Rustam <rustamw888@gmail.com>
2022-06-01 01:38:12 +0300

    temp commit

---
+++ src/test/java/io/qaguru/owner/config/WebDriverConfig.java
@@ -0,0 +1,12 @@
+package io.qaguru.owner.config;
+
+public class WebDriverConfig {
+
+    public Browser getBrowser() {
+        return Browser.FIREFOX;
+    }
+
+    public String getBaseUrl() {
+        return "https://github.com";
+    }
+}
